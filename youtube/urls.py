1d90e0c7daf22b3f1d7f6c16ac48cc382a53b188
Tasneem <tasnim07@gmail.com>
2015-11-30 00:41:03 +0530

    view for listing videos is added

--- youtube/urls.py
+++ youtube/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.google_login, name='google_login'),
     url(r'^callback/$', views.google_login_callback, name='google_login_callback'),
+    url(r'^list_videos/$', views.list_youtube_videos, name='video-list'),
 ]
--- youtube/views.py
+++ youtube/views.py
@@ -5,7 +5,9 @@ import httplib2
 from apiclient.discovery import build
 from oauth2client.file import Storage
 from django.core.urlresolvers import reverse
+from django.http import Http404
 import json
+import requests
 
 def index(request):
     return HttpResponse('Hello world')
@@ -32,3 +34,21 @@ def google_login_callback(request):
 	storage = Storage('credentials')
 	storage.put(credentials)
 	return redirect(reverse('index'))
+
+def list_youtube_videos(request):
+	import pdb; pdb.set_trace()
+	
+	storage = Storage('credentials')
+	credentials = storage.get()
+	access_token = credentials.access_token
+
+	url = 'https://www.googleapis.com/youtube/v3/search'
+	params = {
+		'part': 'snippet',
+		'channelId': 'UCMDV6J2hWXet7ZCfgrXGgeg',
+		'type': 'video',
+		'videoCaption': 'any',
+		'access_token': access_token,		
+	}
+	r = requests.get(url, params=params)
+	return HttpResponse(r.content)
